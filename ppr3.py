4d0795405e3cbefa533702cfebedb6a8831032e0
Paolo 'VoidSec' Stagno <voidsec@voidsec.com>
2022-07-25 23:36:00 +0200

    Update ppr3.py

--- ppr3.py
+++ ppr3.py
@@ -1,46 +1,57 @@
-#!/user/bin/env python3 
-
 """
-POP-POP-RET finder ported in py3 and IDA Pro 7.5 
-Author: Matteo 'uf0' Malvica 
+POP-POP-RET gadget finder ported to Pyhton 3 and IDA Pro 7.7 
+Author: Paolo 'VoidSec' Stagno 
 """
 
+import idaapi
+import idautils
 import idc
-import ida_bytes
 
-def disp(a,b,c,d): 			#Function to display pop/pop/rets...
-	mnem1 = print_operand(a,0)  	#Getting first operand from start addr (pop *)
-	mnem2 = print_operand(int(a+1),0) 	#Getting first operand from start addr +1 (pop xxx, pop *)
-	print("{0:08X}".format(a))
-	y = Assemble(a, str(b+" "+mnem1))[1]  	#Assembling instruction at a
-	a = a+1  	#incrementing a
-	z = Assemble(a, str(c+" "+mnem2))[1]  	#Assembling instruction at a + 1
-	print("{0:08X} {0:08X}".format((ord(y[0])),(ord(z[0]))))  	#Printing assembly - non-mnemonic (e.g. \x5b\x5d\xc3)
+
+def display(start):
+    mnem1 = idc.print_operand(start, 0)
+    mnem2 = idc.print_operand((start + 1), 0)
+    mnem3 = idc.print_operand((start + 2), 0)
+    byte1 = idautils.Assemble(start, "pop " + mnem1)
+    byte2 = idautils.Assemble(start + 1, "pop " + mnem2)
+
+    if mnem3 == "":
+        byte3 = idautils.Assemble(start + 2, "ret")
+    else:
+        byte3 = idautils.Assemble(start + 2, "ret " + mnem3)
+
+    print(
+        f"- 0x{start:08X}: pop {mnem1}; pop {mnem2}; retn {mnem3}; \\x{ord(byte1[1]):02x}\\x{ord(byte2[1]):02x}{byte3[1]}")
+
 
 def main():
-	print("Running POP/POP/RETN Script\n\n")
-	
-	addr = get_segm_by_sel(selector_by_name(".text")) 	# Getting start addr of code segment through the selector for .text
-	end = get_segm_end(addr)  		# Getting end addr of code segment
-	
-	while addr < end and addr != BADADDR:  	#While stepping through addr's and not bad addr's
-		addr = next_addr(addr)  	#addr = next address starting from var addr
-		op1 = print_insn_mnem(addr)  	#Getting mnemonic instruction where addr is pointing
-		if str(op1) == "pop":  	#If the instruction is a pop...
-			x = addr + 1  		#...then incrementing x to the next address after the pop
-			op2 = print_insn_mnem(x) 	#Get the mnemonic instruction of x
-			if str(op2) == "pop":  	#If the instruction is a pop...
-				y = x + 1  		#...then increment x to the next address again.
-				ret = print_insn_mnem(y) 	# Get the instruction at x
-				if str(ret) == "retn":  	#If it's a return....
-					z = get_operand_value(y,0)	#Check to see if the RETN instruction has an operand value. e.g. retn 12. If 
-					if z == -1:	#If it doesn't have an operand value, continue.
-						disp(addr,op1,op2,ret)  #Call the disp() function to display
-				
-	print("\n\nScript Finished!")
+    count = 0
+    print("Executing POP/POP/RET finder...")
+    for ea in idautils.Segments():
+        name = idc.get_segm_name(ea)
+        if name == ".text":
+            start_addr = idc.get_segm_start(ea)
+            end_addr = idc.get_segm_end(ea)
+            print(
+                f"{name} segment start at 0x{start_addr:08X} - end at {end_addr:08X}; size: {end_addr - start_addr} bytes")
+            while start_addr < end_addr and start_addr != idaapi.BADADDR:
+                op1 = idc.print_insn_mnem(start_addr)
+                if op1 == "pop":
+                    x = start_addr + 1
+                    op2 = idc.print_insn_mnem(x)
+                    if op2 == "pop":
+                        y = x + 1
+                        ret = idc.print_insn_mnem(y)
+                        if ret == "retn":
+                            count += 1
+                            display(start_addr)
+                start_addr = idc.next_addr(start_addr)
+    print(f"Finished! Total gadgets found: {count}")
+
 
 if __name__ == '__main__':
     main()
+
 
 
 
